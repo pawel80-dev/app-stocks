d9a6c50f2ede13773978414aa860fdaea75f2454
Pawel <pawel80@gmail.com>
2026-02-10 21:37:55 +0100

    swa_az_func_update_v29

--- api/function_app.py
+++ api/function_app.py
@@ -3,7 +3,7 @@ import logging
 import os
 import azure.functions as func
 from shared.api_some_text import get_some_text, stock_quote
-# from shared.api_twelvedata2 import stock_quote
+from shared.api_twelvedata import stock_list
 
 # Configure logging
 logger = logging.getLogger(__name__)
