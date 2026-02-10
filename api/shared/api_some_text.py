875d35a7ea4098253c6e84b14c95e1f340421d7f
Pawel <pawel80@gmail.com>
2026-02-10 21:15:19 +0100

    swa_az_func_update_v22

--- api/shared/api_some_text.py
+++ api/shared/api_some_text.py
@@ -1,8 +1,8 @@
 import requests
-from urllib3.exceptions import InsecureRequestWarning
 import logging
 import json
 import os
+from urllib3.exceptions import InsecureRequestWarning
 
 # logger will return the source module name
 logger = logging.getLogger(__name__)
@@ -14,3 +14,26 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 def get_some_text():
     return "This is some text from the API."
+
+
+def stock_quote(stock_url: str, api_key: str, symbol: str) -> str:
+    pass
+    # api = f"/quote?symbol={symbol}"
+    # url = stock_url + api
+    # headers = {
+    #     "Authorization": f"apikey {api_key}",
+    #     "Content-Type": "application/json"
+    # }
+    # response = requests.get(
+    #     url=url,
+    #     headers=headers,
+    #     verify=False
+    # )
+    # # if response.json()["code"] == 200:
+    # if response.status_code == 200:
+    #     logger.info(f"Stock {symbol} data successfully retrieved.")
+    #     # return json.dumps(response.json(), indent=4)
+    #     return json.dumps(response.json())
+    # else:
+    #     logger.info(f"Failed to retrieved {symbol} stock data: {response.json()["code"]}")
+    #     logger.info(response.json()["message"])
